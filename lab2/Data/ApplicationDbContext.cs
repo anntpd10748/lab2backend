953de736f2e17893e4dc48d687e295032218e9d4
thanhan <anntpd10748@gmail.com>
2024-11-08 08:56:06 +0700

    lab2

---
+++ lab2/Data/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ServerGame106.Models;
+namespace ServerGame106.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<GameLevel> GameLevels { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<LevelResult> LevelResults { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<GameLevel>().HasData(
+                new GameLevel { LevelID = 1, title = "level 1"},
+                new GameLevel { LevelID = 2, title = "level 2"},
+                new GameLevel { LevelID = 3, title = "level 3"}
+            );
+            modelBuilder.Entity<Region>().HasData(
+                new Region { RegionId = 1, RegionName = "dong bang song hong"},
+                new Region { RegionId =2, RegionName = "dong bang song cuu long"}
+            );
+            modelBuilder.Entity<Question>().HasData(
+                new Question
+                {
+                    QuestionId = 1,
+                    ContentQuestion = "cau hoi 1",
+                    Answer = "dap an 1",
+                    Option1 = "dap an 1",
+                    Option2 = "dap an 2",
+                    Option3 = "dap an 3",
+                    Option4 = "dap an 4",
+                    LevelId = 1
+                },
+                new Question
+                {
+                    QuestionId = 2,
+                    ContentQuestion = "cau hoi 2",
+                    Answer = "dap an 2",
+                    Option1 = "dap an 1",
+                    Option2 = "dap an 2",
+                    Option3 = "dap an 3",
+                    Option4 = "dap an 4",
+                    LevelId = 2
+                }
+            );
+        }
+    }
+    
+}
+    
---
+++ lab2/Migrations/20241107075917_Insert.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ServerGame106.Migrations
+{
+    /// <inheritdoc />
+    public partial class Insert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GameLevels",
+                columns: new[] { "LevelID", "Description", "title" },
+                values: new object[,]
+                {
+                    { 1, null, "level 1" },
+                    { 2, null, "level 2" },
+                    { 3, null, "level 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Questions",
+                columns: new[] { "QuestionId", "Answer", "ContentQuestion", "LevelId", "Option1", "Option2", "Option3", "Option4" },
+                values: new object[,]
+                {
+                    { 1, "dap an 1", "cau hoi 1", 1, "dap an 1", "dap an 2", "dap an 3", "dap an 4" },
+                    { 2, "dap an 2", "cau hoi 2", 2, "dap an 1", "dap an 2", "dap an 3", "dap an 4" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "RegionId", "RegionName" },
+                values: new object[,]
+                {
+                    { 1, "dong bang song hong" },
+                    { 2, "dong bang song cuu long" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GameLevels",
+                keyColumn: "LevelID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "GameLevels",
+                keyColumn: "LevelID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "GameLevels",
+                keyColumn: "LevelID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "RegionId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "RegionId",
+                keyValue: 2);
+        }
+    }
+}
